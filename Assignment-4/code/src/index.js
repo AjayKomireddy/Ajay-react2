4c96440bbe36cb5421a7e446a370fbf0ef9f9983
Ajay Komireddy <ajay.komireddy658@gmail.com>
2021-12-21 12:12:17 +0530

    react4 commit final

--- Assignment-4/code/src/index.js
+++ Assignment-4/code/src/index.js
@@ -1,19 +1,21 @@
-import React from 'react';
+import React from "react";
+
 import ReactDOM from 'react-dom';
 import './index.css';
-import Postview from './Postview';
 import reportWebVitals from './reportWebVitals';
-
-
-
+import Header from './Header';
+import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <Postview />
+      <Header/>  
+        
+      <App/>
   </React.StrictMode>,
   document.getElementById('root')
 );
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
